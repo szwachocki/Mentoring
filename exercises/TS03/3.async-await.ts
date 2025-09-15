76f898540e6b1e5ea3a387698fb427db4387c08b
mbogdanski-sii-pl_puma <mbogdanski@sii.pl>
2025-09-15 08:15:13 +0200

    add additional exercises along with png's

---
+++ exercises/TS03/3.async-await.ts
@@ -0,0 +1,31 @@
+// Twoje zadanie:
+//
+// Poznaj działanie async/await na praktycznym przykładzie!
+//
+// 1. Napisz funkcję asynchroniczną fetchData, która:
+//    - przyjmuje jeden parametr: url (string)
+//    - "symuluje" pobieranie danych z podanego adresu (np. za pomocą setTimeout i Promise)
+//    - po 2 sekundach zwraca tekst: `Dane pobrane z ${url}`
+//    - dodaj komentarze wyjaśniające każdy krok
+//
+// 2. Napisz funkcję asynchroniczną main, która:
+//    - wywołuje fetchData dla url 'https://kurs.ts/async'
+//    - wyświetla wynik w konsoli
+//    - wywołuje fetchData dla url 'https://kurs.ts/await'
+//    - wyświetla wynik w konsoli
+//    - dodaj komentarze wyjaśniające działanie await
+//
+// 3. Wywołaj funkcję main na końcu pliku
+//
+// Oczekiwany rezultat:
+// (po ~2 sek. na konsoli)
+// Dane pobrane z https://kurs.ts/async
+// Dane pobrane z https://kurs.ts/await
+
+// -----------------------TUTAJ UMIEŚĆ SWOJE ROZWIĄZANIE:-------------------------
+
+// Funkcja asynchroniczna, która "symuluje" pobieranie danych
+
+
+// Zignoruj tę linię - umożliwia ponowną deklarację zmiennych w tym projekcie
+export {}
---
+++ exercises/TS03/4.async-await-files.ts
@@ -0,0 +1,30 @@
+// Twoje zadanie:
+//
+// Poznaj async/await na praktycznym przykładzie pracy z plikami!
+//
+// 1. Napisz funkcję asynchroniczną simulateFileRead, która:
+//    - przyjmuje jeden parametr: fileName (string)
+//    - "symuluje" odczyt pliku (np. za pomocą setTimeout i Promise)
+//    - po 1 sekundzie zwraca tekst: `Zawartość pliku ${fileName}`
+//    - dodaj komentarze wyjaśniające każdy krok
+//
+// 2. Napisz funkcję asynchroniczną processFiles, która:
+//    - przyjmuje tablicę nazw plików
+//    - dla każdego pliku wywołuje simulateFileRead z await
+//    - wyświetla zawartość każdego pliku w konsoli
+//    - dodaj komentarze wyjaśniające działanie await w pętli
+//
+// 3. Wywołaj processFiles z tablicą ['plik1.txt', 'plik2.txt', 'plik3.txt']
+//
+// Oczekiwany rezultat:
+// (co sekundę na konsoli)
+// Zawartość pliku plik1.txt
+// Zawartość pliku plik2.txt
+// Zawartość pliku plik3.txt
+
+// -----------------------TUTAJ UMIEŚĆ SWOJE ROZWIĄZANIE:-------------------------
+
+// Funkcja asynchroniczna, która "symuluje" odczyt pliku
+
+// Zignoruj tę linię - umożliwia ponowną deklarację zmiennych w tym projekcie
+export {}
