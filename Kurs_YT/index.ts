9f2122dde9b85e57158de6eebb21d794c3591ca2
szwachocki <szwachocki@gmail.com>
2026-03-23 21:00:27 +0100

    Deklaracja zmiennych

--- Kurs_YT/index.ts
+++ Kurs_YT/index.ts
@@ -1,2 +1,11 @@
 const firstName: string = "John";
-console.log(firstName);
+let ZmiennaJeden = "Słowo bo to sting";
+const ZmiennaDwa = 123;
+let ZmienaTrzy: boolean = true;
+let ZmiennaCztery = 12
+
+console.log("Zmiena z kursu: ", firstName);
+console.log("Moja Jeden: ", ZmiennaJeden);
+console.log("Moja dwa: ", ZmiennaDwa);
+console.log("Moja trzy: ", ZmienaTrzy);
+console.log("sprawdziłem i jest to: ", typeof ZmiennaCztery);
