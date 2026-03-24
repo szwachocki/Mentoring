a17aa8b9846b57bf1e941d289637380e905d9a8b
szwachocki <szwachocki@gmail.com>
2026-03-24 10:10:50 +0100

    Poprawki ex1

--- Kurs_YT/index.ts
+++ Kurs_YT/index.ts
@@ -2,9 +2,9 @@ const firstName: string = "John";
 let ZmiennaJeden = "Słowo bo to sting";
 const ZmiennaDwa = 123;
 let ZmienaTrzy: boolean = true;
-let ZmiennaCztery = 12
+let ZmiennaCztery = true
 
-console.log("Zmiena z kursu: ", firstName);
+console.log("Zmiena z kursu: ", firstName)
 console.log("Moja Jeden: ", ZmiennaJeden);
 console.log("Moja dwa: ", ZmiennaDwa);
 console.log("Moja trzy: ", ZmienaTrzy);
