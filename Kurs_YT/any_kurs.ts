29dc16554c02b7b56c6a27b4b6d75eb08887efe5
szwachocki <szwachocki@gmail.com>
2026-03-25 21:14:55 +0100

    Tu tylko any

---
+++ Kurs_YT/any_kurs.ts
@@ -0,0 +1,17 @@
+console.log("Typ any jest jak dla mnie nielogiczny")
+let FirstAnyVariable: any
+FirstAnyVariable = 1
+console.log("teraz jest to:", typeof FirstAnyVariable)
+FirstAnyVariable = true
+console.log("teraz jest to:", typeof FirstAnyVariable)
+FirstAnyVariable = "jako tako"
+console.log("a teraz :", typeof FirstAnyVariable)
+console.log("Wg mnie to się powinno nadpisywa ;)") // w pierwszej wersji nie użyłem typeof
+
+let AdditionalVariableOne: number
+let AdditionalVariableTwo: string
+const AdditionalVariableThree: boolean = true // tu miałem fajną sytuację bo nie przypisałem od razu wartości do boolean i VSC mi warczał
+
+AdditionalVariableOne=1
+AdditionalVariableTwo="String Variable"
+console.log("Pierwsza", typeof AdditionalVariableOne, "druga", typeof AdditionalVariableTwo, "trzecia", typeof AdditionalVariableThree)
